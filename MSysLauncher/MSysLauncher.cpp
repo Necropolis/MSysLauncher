a95cb4d47e89234ada57d11a00d23737c62c205d
Elder Miller <millercr@familysearch.org>
2011-03-16 08:56:24 -0600

    Initial commit.

---
+++ MSysLauncher/MSysLauncher.cpp
@@ -0,0 +1,13 @@
+// MSysLauncher.cpp : main project file.
+
+#include "stdafx.h"
+
+#include <cstdlib>
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	system("C:\\MinGW\\msys\\1.0\\msys.bat");
+    return 0;
+}
